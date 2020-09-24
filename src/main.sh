af7625447716b1beddebe7e906a3a22b100dbba7
Matej Košiarčik <matej.kosiarcik@gmail.com>
2020-09-24 21:20:26 +0200

    style: fix lint errors

--- src/main.sh
+++ src/main.sh
@@ -12,12 +12,12 @@ help() {
 mode='default'
 if [ "${#}" -ge 1 ]; then
     case "${1}" in
-    '--fast') mode='fast';;
-    '--default') mode='default';;
-    '--brute') mode='brute';;
-    '--help') help && exit 0;;
-    '-h') help && exit 0;;
-    *) printf 'Unrecognized option %s' "${1}\n" && help && exit 1;;
+    '--fast') mode='fast' ;;
+    '--default') mode='default' ;;
+    '--brute') mode='brute' ;;
+    '--help') help && exit 0 ;;
+    '-h') help && exit 0 ;;
+    *) printf 'Unrecognized option %s' "${1}\n" && help && exit 1 ;;
     esac
 fi
 
@@ -31,9 +31,9 @@ printf 'Original: %s\n' "$(wc -c <'0.png')"
 
 printf 'TruePNG...'
 case "${mode}" in
-'fast') truepng '0.png' /o 2 /quiet /y /out '1.png' >/dev/null 2>&1 || true;;
-'default') truepng '0.png' /o 4 /quiet /y /out '1.png' >/dev/null 2>&1 || true;;
-'brute') truepng '0.png' /o max /quiet /y /out '1.png' >/dev/null 2>&1 || true;;
+'fast') truepng '0.png' /o 2 /quiet /y /out '1.png' >/dev/null 2>&1 || true ;;
+'default') truepng '0.png' /o 4 /quiet /y /out '1.png' >/dev/null 2>&1 || true ;;
+'brute') truepng '0.png' /o max /quiet /y /out '1.png' >/dev/null 2>&1 || true ;;
 esac
 printf '\rTruePNG: %s\n' "$(wc -c <'1.png')"
 
@@ -43,16 +43,16 @@ printf '\rPNGOptimizer: %s\n' "$(wc -c <'2.png')"
 
 printf 'OptiPNG...'
 case "${mode}" in
-'brute') optipng -quiet -strip all -o7 -zm1-9 '2.png' -out '3.png';;
-*) optipng -quiet -strip all -o7 '2.png' -out '3.png';;
+'brute') optipng -quiet -strip all -o7 -zm1-9 '2.png' -out '3.png' ;;
+*) optipng -quiet -strip all -o7 '2.png' -out '3.png' ;;
 esac
 printf '\rOptiPNG: %s\n' "$(wc -c <'3.png')"
 
 printf 'ZopfliPNG...'
 case "${mode}" in
-'fast') zopflipng -y --iterations=100 --filters=01234mepb --lossy_8bit --lossy_transparent '3.png' '4.png' >/dev/null 2>&1;;
-'default') zopflipng -y --iterations=250 --filters=01234mepb --lossy_8bit --lossy_transparent '3.png' '4.png' >/dev/null 2>&1;;
-'brute') zopflipng -y --iterations=1000 --filters=01234mepb --lossy_8bit --lossy_transparent '3.png' '4.png' >/dev/null 2>&1;;
+'fast') zopflipng -y --iterations=100 --filters=01234mepb --lossy_8bit --lossy_transparent '3.png' '4.png' >/dev/null 2>&1 ;;
+'default') zopflipng -y --iterations=250 --filters=01234mepb --lossy_8bit --lossy_transparent '3.png' '4.png' >/dev/null 2>&1 ;;
+'brute') zopflipng -y --iterations=1000 --filters=01234mepb --lossy_8bit --lossy_transparent '3.png' '4.png' >/dev/null 2>&1 ;;
 esac
 printf '\rZopfliPNG: %s\n' "$(wc -c <'4.png')"
 
